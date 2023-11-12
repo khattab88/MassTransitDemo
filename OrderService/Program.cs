89b5c3818f7ad43d7adb1b217f38756057395b6e
khattab88 <m_khatab_88@hotmail.com>
2023-11-12 16:08:58 +0200

    Add project files.

---
+++ OrderService/Program.cs
@@ -0,0 +1,60 @@
+
+using MassTransit;
+using Models;
+
+namespace OrderService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            #region basic setup for Masstransit/RabbitMQ
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(config =>
+            {
+                config.Host("amqp://guest:guest@localhost:5672");
+
+                config.ReceiveEndpoint("temp-queue", endpoint =>
+                {
+                    endpoint.Handler<Order>(ctx =>
+                    {
+                        return Console.Out.WriteLineAsync(ctx.Message.Name);
+                    });
+                });
+            });
+
+            bus.Start();
+
+            bus.Publish(new Order { Name = "test order" });
+
+            #endregion
+
+            app.Run();
+        }
+    }
+}
