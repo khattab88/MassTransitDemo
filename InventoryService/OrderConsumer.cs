5faa11cf8da1f5fa28b1caf597692cf90023aa9d
khattab88 <m_khatab_88@hotmail.com>
2023-11-12 16:44:37 +0200

    configure consumer

---
+++ InventoryService/OrderConsumer.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+using Models;
+
+namespace InventoryService
+{
+    public class OrderConsumer : IConsumer<Order>
+    {
+        public async Task Consume(ConsumeContext<Order> context)
+        {
+            await Console.Out.WriteLineAsync(context.Message.Name);
+        }
+    }
+}
--- InventoryService/Program.cs
+++ InventoryService/Program.cs
@@ -1,8 +1,7 @@
 
 using MassTransit;
-using Models;
 
-namespace OrderService
+namespace InventoryService
 {
     public class Program
     {
@@ -11,17 +10,25 @@ namespace OrderService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            // configure masstransit (producer)
+            // configure masstransit (consumer)
             builder.Services.AddMassTransit(options =>
             {
-                options.UsingRabbitMq((ctx, config) => 
+                options.AddConsumer<OrderConsumer>();
+
+                options.UsingRabbitMq((ctx, config) =>
                 {
                     config.Host("amqp://guest:guest@localhost:5672");
+
+                    config.ReceiveEndpoint("order-queue", endpoint =>
+                    {
+                        endpoint.ConfigureConsumer<OrderConsumer>(ctx);
+                    });
                 });
             });
 
@@ -43,27 +50,6 @@ namespace OrderService
 
 
             app.MapControllers();
-
-            #region basic setup for Masstransit/RabbitMQ
-
-            //var bus = Bus.Factory.CreateUsingRabbitMq(config =>
-            //{
-            //    config.Host("amqp://guest:guest@localhost:5672");
-
-            //    config.ReceiveEndpoint("temp-queue", endpoint =>
-            //    {
-            //        endpoint.Handler<Order>(ctx =>
-            //        {
-            //            return Console.Out.WriteLineAsync(ctx.Message.Name);
-            //        });
-            //    });
-            //});
-
-            //bus.Start();
-
-            //bus.Publish(new Order { Name = "test order" });
-
-            #endregion
 
             app.Run();
         }
