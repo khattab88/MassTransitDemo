15683f5437f94cede65540b9630617f4dfd0aec0
khattab88 <m_khatab_88@hotmail.com>
2023-11-12 16:24:56 +0200

    create producer (order service)

---
+++ OrderService/Controllers/OrdersController.cs
@@ -0,0 +1,62 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public OrdersController(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        // GET: api/<OrdersController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<OrdersController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<OrdersController>
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Order order)
+        {
+            try
+            {
+                await _publishEndpoint.Publish<Order>(order);
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        // PUT api/<OrdersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<OrdersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
